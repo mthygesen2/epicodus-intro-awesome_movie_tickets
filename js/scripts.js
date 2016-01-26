665e13b29591c9c19264cc747e7d9c90b5a63c1c
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-26 14:03:56 -0800

    add proper display style for money values

--- js/scripts.js
+++ js/scripts.js
@@ -86,11 +86,14 @@ $(document).ready(function() {
     var priceSubTotal = $('#regularTicketSelection').val()*regularTicket.getPrice() + $('#studentTicketSelection').val()*studentTicket.getPrice() + $('#seniorTicketSelection').val()*seniorTicket.getPrice();
 
     // append ticket DOM chuncks to result column
-    $('#resultList').append('<div class="ticket"><h3 class="movieName">' + movieName + '</h3><p>The movie starts at <span class="movieTime">' + movieTime + '</span></p><p class="ticketNumber">' + 'Regular: ' + ($('#regularTicketSelection').val() + '  Student: ' +  $('#studentTicketSelection').val() + '  Senior: ' + $('#seniorTicketSelection').val()) + '</p><p>This movie is <span class="isMatinee"></span>a matinee.</p><p>This bundle of ticket(s) costs $' + priceSubTotal + '</p></div>');
+    $('#resultList').append('<div class="ticket"><h3 class="movieName">' + movieName + '</h3><p>The movie starts at <span class="movieTime">' + movieTime + '</span></p><p class="ticketNumber">' + 'Regular: ' + ($('#regularTicketSelection').val() + '  Student: ' +  $('#studentTicketSelection').val() + '  Senior: ' + $('#seniorTicketSelection').val()) + '</p><p>This movie is <span class="isMatinee">not </span>a matinee.</p><p>This bundle of ticket(s) costs $' + priceSubTotal.toFixed(2) + '</p></div>');
     $('#resultFooter').show();
+    if (isMatinee) {
+      $('.isMatinee').last().hide();
+    }
 
     // insert total cost into html
-    $('#totalCostSpan').text(siteUser.getTotalPrice());
+    $('#totalCostSpan').text(siteUser.getTotalPrice().toFixed(2));
   });
 
   // event handler for reset button
