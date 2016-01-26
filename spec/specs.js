ae63d372b331286de8a8c58f9a39a9f27a8d18f8
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-26 08:34:59 -0800

    pass all object creation specs, constructor creates object with correct properties

--- spec/specs.js
+++ spec/specs.js
@@ -3,20 +3,20 @@ describe('Ticket', function() {
     var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
     expect(testTicket.movieName).to.equal('Jaws');
   });
-  // it('Ticket creates an object with the right time', function() {
-  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
-  //   expect(testTicket.movieTime).to.equal('7:15');
-  // });
-  // it('Ticket creates an object with the right', function() {
-  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
-  //   expect(testTicket.moviePurchaser).to.equal('regular');
-  // });
-  // it('Ticket creates an object with the right', function() {
-  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
-  //   expect(testTicket.isMatinee).to.equal(false);
-  // });
-  // it('Ticket creates an object with the right', function() {
-  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
-  //   expect(testTicket.isNewRelease).to.equal(false);
-  // });
+  it('Ticket creates an object with the right time', function() {
+    var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+    expect(testTicket.movieTime).to.equal('7:15');
+  });
+  it('Ticket creates an object with the right purchaser type', function() {
+    var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+    expect(testTicket.moviePurchaser).to.equal('regular');
+  });
+  it('Ticket creates an object with the right matinee boolean', function() {
+    var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+    expect(testTicket.isMatinee).to.equal(false);
+  });
+  it('Ticket creates an object with the right new release boolean', function() {
+    var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+    expect(testTicket.isNewRelease).to.equal(false);
+  });
 });
