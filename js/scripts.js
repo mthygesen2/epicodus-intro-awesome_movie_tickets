b0a23a421c659c6a21b4e82440b0779ee437ee18
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-26 08:55:41 -0800

    passes specs for method getMatineeDiscount

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// ===============================
+// Business Logic
+// ===============================
 function Ticket(movieName, movieTime, moviePurchaser, isMatinee, isNewRelease) {
   this.movieName = movieName;
   this.movieTime = movieTime;
@@ -5,3 +8,28 @@ function Ticket(movieName, movieTime, moviePurchaser, isMatinee, isNewRelease) {
   this.isMatinee = isMatinee;
   this.isNewRelease = isNewRelease;
 }
+Ticket.prototype.getPrice = function() {
+
+}
+Ticket.prototype.getMatineeDiscount = function() {
+  if (this.isMatinee) {
+    return -0.5;
+  }
+  return 0;
+}
+Ticket.prototype.getPrice = function() {
+
+}
+
+
+
+
+
+
+
+
+
+
+// ===============================
+// User Interface Logic
+// ===============================
