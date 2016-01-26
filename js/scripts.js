09608371823dc58a1bc752f7e37a9a9b0e5cf4e0
Blair Peterson and Joseph Karasek <blairchalmers@gmail.com>
2016-01-26 08:32:33 -0800

    pass first spec, Ticket constructor makes object with correct name property

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Ticket(movieName, movieTime, moviePurchaser, isMatinee, isNewRelease) {
+  this.movieName = movieName;
+  this.movieTime = movieTime;
+  this.moviePurchaser = moviePurchaser;
+  this.isMatinee = isMatinee;
+  this.isNewRelease = isNewRelease;
+}
---
+++ spec/specs.js
@@ -0,0 +1,22 @@
+describe('Ticket', function() {
+  it('Ticket creates an object with the right movie name', function() {
+    var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+    expect(testTicket.movieName).to.equal('Jaws');
+  });
+  // it('Ticket creates an object with the right time', function() {
+  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+  //   expect(testTicket.movieTime).to.equal('7:15');
+  // });
+  // it('Ticket creates an object with the right', function() {
+  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+  //   expect(testTicket.moviePurchaser).to.equal('regular');
+  // });
+  // it('Ticket creates an object with the right', function() {
+  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+  //   expect(testTicket.isMatinee).to.equal(false);
+  // });
+  // it('Ticket creates an object with the right', function() {
+  //   var testTicket = new Ticket("Jaws", "7:15", "regular", false, false);
+  //   expect(testTicket.isNewRelease).to.equal(false);
+  // });
+});
